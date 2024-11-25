1cc95bdd621480fa70e97f7f75bb14697389cb6f
Eka Winata <eka.winata@gopay.co.id>
2024-11-25 10:51:46 +0700

    test: Add test for RetryUtils.java

---
+++ src/test/java/com/gotocompany/depot/utils/RetryUtilsTest.java
@@ -0,0 +1,55 @@
+package com.gotocompany.depot.utils;
+
+import com.gotocompany.depot.exception.NonRetryableException;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class RetryUtilsTest {
+
+    @Test
+    public void shouldRetryUntilSuccess() throws Exception {
+        int repeatCountBeforeSuccess = 3;
+        RunnableMock runnableMock = Mockito.spy(new RunnableMock(repeatCountBeforeSuccess));
+
+        RetryUtils.executeWithRetry(runnableMock::execute, 5, 0, Exception.class);
+
+        Mockito.verify(runnableMock, Mockito.times(repeatCountBeforeSuccess)).execute();
+    }
+
+    @Test(expected = NonRetryableException.class)
+    public void shouldThrowNonRetryableExceptionAfterRetryIsExhausted() throws Exception {
+        int repeatCountBeforeSuccess = 5;
+        RunnableMock runnableMock = Mockito.spy(new RunnableMock(repeatCountBeforeSuccess));
+
+        RetryUtils.executeWithRetry(runnableMock::execute, 3, 0, RuntimeException.class);
+
+        Mockito.verify(runnableMock, Mockito.times(3)).execute();
+    }
+
+    @Test(expected = NonRetryableException.class)
+    public void shouldThrowNonRetryableExceptionWhenNonMatchingExceptionIsThrown() throws Exception {
+        RunnableMock runnableMock = Mockito.spy(new RunnableMock(3));
+
+        RetryUtils.executeWithRetry(runnableMock::execute, 3, 0, IllegalArgumentException.class);
+
+        Mockito.verify(runnableMock, Mockito.times(1)).execute();
+    }
+
+    private static class RunnableMock {
+        private final int repeatCountBeforeSuccess;
+        private int repeatCount;
+
+        RunnableMock(int repeatCountBeforeSuccess) {
+            this.repeatCountBeforeSuccess = repeatCountBeforeSuccess;
+            this.repeatCount = 0;
+        }
+
+        void execute() throws Exception {
+            repeatCount++;
+            if (repeatCount < repeatCountBeforeSuccess) {
+                throw new Exception("Mock exception");
+            }
+        }
+    }
+
+}
