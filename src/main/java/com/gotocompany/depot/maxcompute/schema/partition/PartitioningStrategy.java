6a8a850f611b1936b2c0fe7b2fbd5a4f7cb654c2
Eka Winata <eka.winata@gopay.co.id>
2024-11-25 20:23:57 +0700

    chore: checkstyle main

--- src/main/java/com/gotocompany/depot/maxcompute/schema/partition/PartitioningStrategy.java
+++ src/main/java/com/gotocompany/depot/maxcompute/schema/partition/PartitioningStrategy.java
@@ -2,7 +2,6 @@ package com.gotocompany.depot.maxcompute.schema.partition;
 
 import com.aliyun.odps.Column;
 import com.aliyun.odps.PartitionSpec;
-import com.gotocompany.depot.maxcompute.schema.MaxComputeSchemaCache;
 
 public interface PartitioningStrategy {
     String getOriginalPartitionColumnName();
