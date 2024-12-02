d1aff87a19717e7ce94c0e7d93c96bda23a7c958
Eka Winata <eka.winata@gopay.co.id>
2024-12-02 10:57:01 +0700

    chore: add javadoc for PayloadConverter

--- src/main/java/com/gotocompany/depot/maxcompute/converter/payload/PayloadConverter.java
+++ src/main/java/com/gotocompany/depot/maxcompute/converter/payload/PayloadConverter.java
@@ -5,8 +5,19 @@ import com.google.protobuf.Descriptors;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Interface for converting payloads based on field descriptors.
+ */
 public interface PayloadConverter {
 
+    /**
+     * Converts the given object based on the field descriptor.
+     * If the field is repeated, it converts each element in the list.
+     *
+     * @param fieldDescriptor the field descriptor
+     * @param object the object to convert
+     * @return the converted object
+     */
     default Object convert(Descriptors.FieldDescriptor fieldDescriptor, Object object) {
         if (!fieldDescriptor.isRepeated()) {
             return convertSingular(fieldDescriptor, object);
@@ -16,7 +27,20 @@ public interface PayloadConverter {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Converts a singular object based on the field descriptor.
+     *
+     * @param fieldDescriptor the field descriptor
+     * @param object the object to convert
+     * @return the converted object
+     */
     Object convertSingular(Descriptors.FieldDescriptor fieldDescriptor, Object object);
 
+    /**
+     * Checks if the converter can convert the given field descriptor.
+     *
+     * @param fieldDescriptor the field descriptor
+     * @return true if the converter can convert the field descriptor, false otherwise
+     */
     boolean canConvert(Descriptors.FieldDescriptor fieldDescriptor);
 }
