15c1944e348c8fe19a3e58f0520811881cf0e8dc
Eka Winata <eka.winata@gopay.co.id>
2024-12-11 23:28:57 +0700

    refactor: remove proxy MaxComputeSinkConfig to TimestampProtobufMaxComputeConverter

--- src/main/java/com/gotocompany/depot/maxcompute/converter/TimestampProtobufMaxComputeConverter.java
+++ src/main/java/com/gotocompany/depot/maxcompute/converter/TimestampProtobufMaxComputeConverter.java
@@ -11,6 +11,7 @@ import com.gotocompany.depot.maxcompute.model.ProtoPayload;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.temporal.TemporalAmount;
 
@@ -22,14 +23,26 @@ public class TimestampProtobufMaxComputeConverter implements ProtobufMaxComputeC
     private static final String NANOS = "nanos";
     private static final long DAYS_IN_YEAR = 365;
 
-    private final MaxComputeSinkConfig maxComputeSinkConfig;
     private final TemporalAmount maxPastEventTimeDifference;
     private final TemporalAmount maxFutureEventTimeDifference;
+    private final ZoneId zoneId;
+    private final LocalDateTime validMinTimestamp;
+    private final LocalDateTime validMaxTimestamp;
+    private final boolean isTablePartitioningEnabled;
+    private final String tablePartitionKey;
+    private final int maxPastYearEventTimeDifference;
+    private final int maxFutureYearEventTimeDifference;
 
     public TimestampProtobufMaxComputeConverter(MaxComputeSinkConfig maxComputeSinkConfig) {
-        this.maxComputeSinkConfig = maxComputeSinkConfig;
         this.maxPastEventTimeDifference = Duration.ofDays(maxComputeSinkConfig.getMaxPastYearEventTimeDifference() * DAYS_IN_YEAR);
         this.maxFutureEventTimeDifference = Duration.ofDays(maxComputeSinkConfig.getMaxFutureYearEventTimeDifference() * DAYS_IN_YEAR);
+        this.zoneId = maxComputeSinkConfig.getZoneId();
+        this.validMinTimestamp = maxComputeSinkConfig.getValidMinTimestamp();
+        this.validMaxTimestamp = maxComputeSinkConfig.getValidMaxTimestamp();
+        this.isTablePartitioningEnabled = maxComputeSinkConfig.isTablePartitioningEnabled();
+        this.tablePartitionKey = maxComputeSinkConfig.getTablePartitionKey();
+        this.maxPastYearEventTimeDifference = maxComputeSinkConfig.getMaxPastYearEventTimeDifference();
+        this.maxFutureYearEventTimeDifference = maxComputeSinkConfig.getMaxFutureYearEventTimeDifference();
     }
 
     @Override
@@ -49,7 +62,7 @@ public class TimestampProtobufMaxComputeConverter implements ProtobufMaxComputeC
         long seconds = (long) message.getField(message.getDescriptorForType().findFieldByName(SECONDS));
         int nanos = (int) message.getField(message.getDescriptorForType().findFieldByName(NANOS));
         Instant instant = Instant.now();
-        ZoneOffset zoneOffset = maxComputeSinkConfig.getZoneId().getRules().getOffset(instant);
+        ZoneOffset zoneOffset = zoneId.getRules().getOffset(instant);
         LocalDateTime localDateTime = LocalDateTime.ofEpochSecond(seconds, nanos, zoneOffset);
         validateTimestampRange(localDateTime);
         validateTimestampPartitionKey(protoPayload.getFieldDescriptor().getName(), localDateTime, protoPayload.isRootLevel());
@@ -57,28 +70,28 @@ public class TimestampProtobufMaxComputeConverter implements ProtobufMaxComputeC
     }
 
     private void validateTimestampRange(LocalDateTime localDateTime) {
-        if (localDateTime.isBefore(maxComputeSinkConfig.getValidMinTimestamp()) || localDateTime.isAfter(maxComputeSinkConfig.getValidMaxTimestamp())) {
+        if (localDateTime.isBefore(validMinTimestamp) || localDateTime.isAfter(validMaxTimestamp)) {
             throw new InvalidMessageException(String.format("Timestamp %s is out of allowed range range min: %s max: %s",
-                    localDateTime, maxComputeSinkConfig.getValidMinTimestamp(), maxComputeSinkConfig.getValidMaxTimestamp()));
+                    localDateTime, validMinTimestamp, validMaxTimestamp));
         }
     }
 
     private void validateTimestampPartitionKey(String fieldName, LocalDateTime eventTime, boolean isRootLevel) {
-        if (!maxComputeSinkConfig.isTablePartitioningEnabled()) {
+        if (!isTablePartitioningEnabled) {
             return;
         }
         if (!isRootLevel) {
             return;
         }
-        if (fieldName.equals(maxComputeSinkConfig.getTablePartitionKey())) {
+        if (fieldName.equals(tablePartitionKey)) {
             Instant now = Instant.now();
-            Instant eventTimeInstant = eventTime.toInstant(maxComputeSinkConfig.getZoneId().getRules().getOffset(now));
+            Instant eventTimeInstant = eventTime.toInstant(zoneId.getRules().getOffset(now));
 
             if (now.minus(maxPastEventTimeDifference).isAfter(eventTimeInstant)) {
-                throw new InvalidMessageException(String.format("Timestamp is in the past, you can only stream data within %d year(s) in the past", maxComputeSinkConfig.getMaxPastYearEventTimeDifference()));
+                throw new InvalidMessageException(String.format("Timestamp is in the past, you can only stream data within %d year(s) in the past", maxPastYearEventTimeDifference));
             }
             if (now.plus(maxFutureEventTimeDifference).isBefore(eventTimeInstant)) {
-                throw new InvalidMessageException(String.format("Timestamp is in the future, you can only stream data within %d year(s) in the future", maxComputeSinkConfig.getMaxFutureYearEventTimeDifference()));
+                throw new InvalidMessageException(String.format("Timestamp is in the future, you can only stream data within %d year(s) in the future", maxFutureYearEventTimeDifference));
             }
         }
     }
